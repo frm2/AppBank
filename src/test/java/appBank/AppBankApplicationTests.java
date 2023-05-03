eea9b49381287ef883162d15c1378929b422470c
Fernando Rangel Magalhães <fernandorangelm96@gmail.com>
2023-05-03 16:07:25 -0300

    Set the environment

---
+++ src/test/java/appBank/AppBankApplicationTests.java
@@ -0,0 +1,13 @@
+package appBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
